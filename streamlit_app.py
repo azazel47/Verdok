64aac6295b7eb8d1c2eb9ad0541d5a0a8fe354a1
azazel47 <49670061+azazel47@users.noreply.github.com>
2025-07-30 15:58:32 +0700

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -124,10 +124,8 @@ def download_sedimentasi_shapefile():
         st.warning(f"Gagal mengunduh dan membaca shapefile Sedimentasi: {e}")
         return None
 
-def load_local_shapefile(filepath_without_ext):
-    try:
-        gdf = gpd.read_file(f"{filepath_without_ext}.shp")
-        return gdf
+def load_local_shapefile(shp_path):
+    return gpd.read_file(shp_path)
     except Exception as e:
         st.warning(f"Gagal membaca shapefile lokal {filepath_without_ext}: {e}")
         return None
@@ -170,8 +168,8 @@ konservasi_gdf = get_kawasan_konservasi_from_arcgis()
 mil12_gdf = download_shapefile_from_gdrive("https://drive.google.com/file/d/16MnH27AofcSSr45jTvmopOZx4CMPxMKs/view?usp=sharing")
 sedimen_gdf = download_sedimentasi_shapefile() if cek_sedimentasi else None
 kkprl_gdf = load_kkprl_json()
-tambang_gdf = load_local_shapefile("data/IUP_FULL_INDO") if cek_pertambangan else None
-migas_gdf = load_local_shapefile("data/MIGAS_FULL_INDO") if cek_migas else None
+tambang_gdf = load_local_shapefile("data/IUP_FULL_INDO.shp") if cek_pertambangan else None
+migas_gdf = load_local_shapefile("data/MIGAS_FULL_INDO.shp") if cek_migas else None
 rumpon_gdf = download_shapefile_from_gdrive("https://drive.google.com/file/d/1vUQArCq7A6iDEJ3AHyMAOtONAMYR2dI-/view?usp=sharing") if cek_rumpon else None
 
 if uploaded_file and nama_file:
